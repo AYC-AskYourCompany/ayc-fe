b05ef5e1bcd829700bfa16061bae317af067d230
msauter@proqrent.de <Bernhard123>
2020-12-21 17:13:37 +0100

    Remove empty lines

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -13,5 +13,4 @@ export class NavbarComponent {
   logout(): void {
     this.keycloakService.logout();
   }
-
 }
--- src/app/shared/navbar/profile-button/profile-button.component.ts
+++ src/app/shared/navbar/profile-button/profile-button.component.ts
@@ -14,5 +14,4 @@ export class ProfileButtonComponent {
   logout(): void {
     this.logoutEvent.emit();
   }
-
 }
